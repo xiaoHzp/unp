934a379a8a0287399ade8b2a9410f409232665c1
adtim <931152507@qq.com>
2017-06-04 17:53:56 +0800

    add the server

---
+++ ch1/ex1_2_server.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+typedef struct sockaddr SA;
+int main(int argc,char* argv[])
+{
+	int listenfd,connfd;
+	struct sockaddr_in servaddr;
+	char buff[4096];
+	time_t ticks;
+
+	listenfd = socket(AF_INET,SOCK_STREAM,0);
+
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(13);
+
+	bind(listenfd,(SA*)&servaddr,sizeof(servaddr));
+
+	while(1)
+	{
+		connfd = accept(listenfd,(SA*)NULL,NULL);
+		ticks = time(NULL);
+		snprintf(buff,sizeof(buff),"%.24s\r\n",ctime(&ticks));
+		write(connfd,buff,strlen(buff));
+		close(connfd);
+	}
+}
